aba998c58d56f4ce4205770cc934ae1bedd2d6e7
fuwa0112 <fuwa0112@icloud.com>
2024-06-07 21:58:14 +0900

    advance models2

---
+++ src/models2_advance_.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops.layers.torch import Rearrange
+
+
+class BasicConvClassifier(nn.Module):
+    def __init__(
+        self,
+        num_classes: int,
+        seq_len: int,
+        in_channels: int,
+        hid_dim: int = 128,
+        num_blocks: int = 4,
+        kernel_size: int = 3,
+        p_drop: float = 0.3
+    ) -> None:
+        super().__init__()
+
+        self.blocks = nn.Sequential(
+            *[ConvBlock(in_channels if i == 0 else hid_dim, hid_dim, kernel_size, p_drop) for i in range(num_blocks)]
+        )
+
+        self.head = nn.Sequential(
+            nn.AdaptiveAvgPool1d(1),
+            Rearrange("b d 1 -> b d"),
+            nn.Linear(hid_dim, num_classes),
+        )
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        X = self.blocks(X)
+        return self.head(X)
+
+
+class ConvBlock(nn.Module):
+    def __init__(
+        self,
+        in_dim,
+        out_dim,
+        kernel_size: int = 3,
+        p_drop: float = 0.1,
+    ) -> None:
+        super().__init__()
+        
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+        self.conv0 = nn.Conv1d(in_dim, out_dim, kernel_size, padding="same")
+        self.conv1 = nn.Conv1d(out_dim, out_dim, kernel_size, padding="same")
+        self.batchnorm0 = nn.BatchNorm1d(num_features=out_dim)
+        self.batchnorm1 = nn.BatchNorm1d(num_features=out_dim)
+
+        self.dropout = nn.Dropout(p_drop)
+        self.ln = nn.LayerNorm(out_dim)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        if self.in_dim == self.out_dim:
+            X = self.conv0(X) + X  # skip connection
+        else:
+            X = self.conv0(X)
+
+        X = F.leaky_relu(self.batchnorm0(X))
+        X = self.conv1(X) + X  # skip connection
+        X = F.leaky_relu(self.batchnorm1(X))
+
+        return self.dropout(X)
