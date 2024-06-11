1b133760cfe0b9b2b5a93fd41faee6ac442e28ca
fuwa0112 <fuwa0112@icloud.com>
2024-06-11 13:39:59 +0900

    subject

--- main02_subject.py
+++ main02_subject.py
@@ -9,7 +9,8 @@ import wandb
 from termcolor import cprint
 from tqdm import tqdm
 
-from src.datasets_preprocess import ThingsMEGDataset
+#from src.datasets_preprocess import ThingsMEGDataset
+from src.datasets import ThingsMEGDataset
 from src.models import BasicConvClassifier
 from src.models2_layer_increase import BasicConvClassifier2
 from src.models3_LSTM import BasicConvClassifier3
